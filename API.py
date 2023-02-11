e2eecde64d7100269d60a692d32d7666a7560e7c
Lwa-15 <lwa15@hotmail.co.uk>
2023-02-11 15:03:41 +0000

    Refactored the getAccounts functions into one function

--- API.py
+++ API.py
@@ -23,35 +23,18 @@ class API:
         response = requests.post("https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/accounts/create", headers=self.headers, data=payload)
         return response.status_code
 
-    #Returns two accounts who possess good credit scores 
-    def getGoodCreditAccounts(self):
+    #Returns relevant data from an account based on the parameter
+    #This function was refactored from two functions, one returning data from an account that had a 'Good' credit score and another returning data from an account that had a 'Bad' credit score 
+    def getAccountData(self, parameter):
         #The FICO Credit Scores Ranges will be used in this application - information about these ranges was found on Capital One "What Is a Credit Score Range?" (https://www.capitalone.com/learn-grow/money-management/credit-score-ranges/)
         #A 'Good' Credit Score is more than or equal to 580 ('Fair' or better), according to the FICO Credit Scores Ranges
+        #A 'Bad' Credit Score is less than 580 (less than 'Fair'), according to the FICO Credit Scores Ranges
         params = {
-            'creditScore': 'gte:580',
+            "creditScore": parameter
         }
 
         response = requests.get("https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/accounts", headers=self.headers, params=params)
         response_statusCode = response.status_code
-        response_JSON = response.json()["Accounts"]
-        account_1 = response_JSON[0]
-        account_2 = response_JSON[1]
-        account_1_object = {"balance":account_1["balance"], "creditScore": account_1["creditScore"], "currencyCode":account_1["currencyCode"], "riskScore":account_1["riskScore"], "state": account_1["state"]}
-        account_2_object = {"balance":account_2["balance"], "creditScore": account_2["creditScore"], "currencyCode":account_2["currencyCode"], "riskScore":account_2["riskScore"], "state": account_2["state"]}
-
-        returningObject = {"status": response_statusCode, "account_1": account_1_object, "account_2": account_2_object}
-        return returningObject
-
-
-    #Returns an account that possesses a bad credit score
-    def getBadCreditAccounts(self):
-        #A 'Bad' Credit Score is less than 580 (less than 'Fair'), according to the FICO Credit Scores Ranges
-        params = {
-            'creditScore': 'lt:580',
-        }
-
-        response = requests.get("https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/accounts", headers=self.headers, params=params)
-        response_statusCode = response.status_code #The status code will be included in the returning object to ensure a 200 was returned, handling the error if not
         response_JSON = response.json()["Accounts"]
         account = response_JSON[0]
         account_object = {"balance":account["balance"], "creditScore": account["creditScore"], "currencyCode":account["currencyCode"], "riskScore":account["riskScore"], "state": account["state"]}
@@ -64,4 +47,7 @@ JWT=os.getenv("JWT")
 
 #Testing section
 api = API(JWT)
-print(api.getBadCreditAccounts())
+print("Good:")
+print(api.getAccountData("gte:580"))
+print("Bad:")
+print(api.getAccountData("lt:580"))
