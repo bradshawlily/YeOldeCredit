b6c481083d7d9a1a3d0e993f16955f9fbb4e84bb
N0862504 <N0862504@my.ntu.ac.uk>
2023-02-12 00:44:05 +0000

    Created scene 2

---
+++ Scene2.py
@@ -0,0 +1,22 @@
+import pygame
+pygame.init()
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 800
+
+Scene_Screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+Scene_Screen.fill((0, 0, 0))  # Fills screen with specified colour
+pygame.display.set_caption("Scene Two")
+
+background = pygame.image.load('Sprites/CastleBackground.png')
+background = pygame.transform.scale(background, (800, 800))
+rect = background.get_rect()
+
+while True:
+    Scene_Screen.fill(0, 0, 0)
+    Scene_Screen.blit(background, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        pygame.display.update()
