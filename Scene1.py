138ce2ac54c34949f0565b2607c2a7b78cc78744
Lwa-15 <lwa15@hotmail.co.uk>
2023-02-12 02:52:14 +0000

    Scene 1 in progress and placeholders in scene

--- Scene1.py
+++ Scene1.py
@@ -9,17 +9,30 @@ Scene_Screen.fill((0, 0, 0))  # Fills screen with specified colour
 pygame.display.set_caption("Scene One")
 
 background = pygame.image.load('Sprites/FieldBckgrnd.png')
+villager = pygame.image.load("Sprites/villager_noTransparent.png")
+speechBubble = pygame.image.load("Sprites/SpeechBubble.png")
 #Method to scale the background to the size of the image was found on CodeSpeedy "Scale image to fit screen in Pygame Python" (https://www.codespeedy.com/scale-image-to-fit-screen-in-pygame-python/)
 background = pygame.transform.scale(background, (SCREEN_HEIGHT, SCREEN_WIDTH))
-rect = background.get_rect()
+villager = pygame.transform.scale(villager, (350, 300))
+hero = pygame.transform.scale(villager, (350, 300))
+speechBubble = pygame.transform.scale(speechBubble, (300, 200))
+#Method to flip an image was found on GeeksForGeeks "Pygame – Flip the image" (https://www.geeksforgeeks.org/pygame-flip-the-image/)
+villager = pygame.transform.flip(villager, True, False)
+speechBubble = pygame.transform.flip(speechBubble, True, False)
+background_rect = background.get_rect()
 
 # Game loop
 while True:
     Scene_Screen.fill((0, 0, 0))
-    Scene_Screen.blit(background, rect)
-    
+    Scene_Screen.blit(background, background_rect)
+    Scene_Screen.blit(villager, (500,100))
+    Scene_Screen.blit(hero, (50,100))
+    Scene_Screen.blit(speechBubble, (350, 50))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        Scene_Screen.blit(background, (0,0))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
+
         pygame.display.update()
