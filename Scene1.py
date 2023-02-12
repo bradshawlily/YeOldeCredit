31abe351d7873fd13cca7e0492f7ce3c96aa8dd9
Lwa-15 <lwa15@hotmail.co.uk>
2023-02-12 02:05:49 +0000

    Start Page done

--- Scene1.py
+++ Scene1.py
@@ -9,16 +9,17 @@ Scene_Screen.fill((0, 0, 0))  # Fills screen with specified colour
 pygame.display.set_caption("Scene One")
 
 background = pygame.image.load('Sprites/FieldBckgrnd.png')
-background = pygame.transform.scale(background, (800, 800))
+#Method to scale the background to the size of the image was found on CodeSpeedy "Scale image to fit screen in Pygame Python" (https://www.codespeedy.com/scale-image-to-fit-screen-in-pygame-python/)
+background = pygame.transform.scale(background, (SCREEN_HEIGHT, SCREEN_WIDTH))
 rect = background.get_rect()
 
 # Game loop
 while True:
-
     Scene_Screen.fill((0, 0, 0))
-    Scene_Screen.blit(background, (0, 0))
-
+    Scene_Screen.blit(background, rect)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        Scene_Screen.blit(background, (0,0))
         pygame.display.update()
